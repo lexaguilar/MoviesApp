3c28532e3fcf3b332ca848ed011ffa595094bcb3
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-12-16 01:04:40 -0600

    Creando el Contexto para manejar estados

---
+++ src/context/DataContext.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState }  from 'react';
+
+export const DataContext = createContext();
+
+export const DataProvider = ({  children }) => {
+    
+    const [ favorites, setFavorites ] = useState([]);
+
+    return(
+        <DataContext.Provider value={{favorites, setFavorites}}>
+            {children}
+        </DataContext.Provider>
+    )
+
+}
+
