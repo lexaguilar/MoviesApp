33a908eabd18d84cb661b6b741f89f9ad636ac8f
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-12-16 08:59:06 -0600

    importar los componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Favorites from './components/favorites';
+import Movies from './components/movies';
+import SearchBar from './components/searchBar/SearchBar';
+import { DataProvider } from './context/DataContext';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <DataProvider>
+      <div className="container">
+        <SearchBar />
+        <Movies />
+        <Favorites />
+      </div>
+    </DataProvider>
   );
 }
 
