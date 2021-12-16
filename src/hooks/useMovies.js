61a36de5fe929a79356dc4366b64138ba5c66fce
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-12-16 08:57:56 -0600

    Agregar parametros al hook useMovie

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import http from '../utils/http';
 
-const useMovies = ({ title_like='', genders = [], initDate, endDate, limit=5, page=1 }) => {
+const useMovies = ({ title_like='', genders = [], initDate, endDate, _limit=5, _page=0 }) => {
 
     const [data, setData] = useState([]);
 
     const callApi = async () => {
 
-        const resp = await http('movies').asGet({ title_like });
+        const resp = await http('movies').asGet({ title_like, _page, _limit  });
 
         setData(resp);
 
