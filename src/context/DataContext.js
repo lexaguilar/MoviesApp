c665a33b7c76ae43949d7cf1cff5cdf8c17ba1a8
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-12-16 08:59:28 -0600

    agregar estado query al contexto

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -5,9 +5,10 @@ export const DataContext = createContext();
 export const DataProvider = ({  children }) => {
     
     const [ favorites, setFavorites ] = useState([]);
+    const [ query, setQuery ] = useState('');
 
     return(
-        <DataContext.Provider value={{favorites, setFavorites}}>
+        <DataContext.Provider value={{favorites, setFavorites, query, setQuery}}>
             {children}
         </DataContext.Provider>
     )
